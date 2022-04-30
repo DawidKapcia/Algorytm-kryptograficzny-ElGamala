5f8a95b9bdae52d586c4d2c05349cbe55f79db46
DawidKapcia <104532717+DawidKapcia@users.noreply.github.com>
2022-04-30 23:33:32 +0200

    Delete key_generator.py

--- key_generator.py
+++
@@ -1,9 +0,0 @@
-def key_generator():
-    
-    p = prime(a, b)
-    alpha = alpha_generator(p)
-    a = random.randint(1, p-2)
-    
-    a_alpha = mod_power(alpha, a, p)
-    
-    return p, alpha, a_alpha, a
