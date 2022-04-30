5234cb78b6631db3b6e699f6d5dac5ce5023dcfc
DawidKapcia <104532717+DawidKapcia@users.noreply.github.com>
2022-04-30 23:35:43 +0200

    Create elgamal.py

---
+++ elgamal.py
@@ -0,0 +1,43 @@
+def key():
+    
+    p = prime(pocz, kon)
+    alpha = generator_alpha(p)
+    a = random.randint(1, p-2)
+    
+    a_alpha = mod_power(alpha, a, p)
+    
+    return p, alpha, a_alpha, a
+
+
+def encryption(m, p, alpha, a_alpha):
+ 
+    k = random.randint(1, p-2)
+    
+    gamma = mod_power(alpha, k, p)
+    delta = m * mod_power(a_alpha, k, p)
+ 
+    return gamma, delta
+
+
+def decryption(gamma, delta, p, a):
+    
+    pom = mod_power(gamma, p-1-a, p)
+    m = (pom * delta) % p
+    
+    return m
+
+
+def ElGamal(m, a, b):
+    
+    sep = 60 * "-"
+    
+    print("%s\nInput messege:\t%s\n%s" % (sep, m, sep))
+    
+    p, alpha, a_alpha, a = key()
+    print("\nPublic key:\n\np = %s\nalpha = %s\na_alpha = %s\na = %s\n\n%s" % (p, alpha, a_alpha, sep))
+    
+    gamma, delta = encryption(m, p, alpha, a_alpha)
+    print("\nEncrypted message:\n\n(%s, %s)\n\n%s" % (gamma, delta, sep))
+    
+    m = decryption(gamma, delta, p, a)
+    print("\nDecrypted message:\n\n%s\n\n%s" % (m, sep))
