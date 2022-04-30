48cd2e9419a51f013cafe7dd58da0038661d6feb
DawidKapcia <104532717+DawidKapcia@users.noreply.github.com>
2022-04-30 23:28:42 +0200

    Create functions.py

---
+++ functions.py
@@ -0,0 +1,38 @@
+import random
+import sympy
+
+def mod_power(a, b, c):
+    
+    x = 1
+    y = a
+ 
+    while b > 0:
+        
+        if b % 2 != 0:
+            x = (x * y) % c
+            
+        y = (y * y) % c
+        b = b // 2
+ 
+    return x % c
+
+
+
+def prime(a, b):
+    
+    p = random.randint(a, b)
+    
+    while not sympy.isprime(p):
+        p = random.randint(a, b)
+    
+    return p
+  
+
+  
+def generator_alpha(p):
+    
+    while True:
+        alpha = random.randint(2, p-1)
+        
+        if (p-1) % alpha != 1:
+            return alpha
